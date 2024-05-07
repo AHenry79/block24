7990c2a27cfd92e7043d9e81c6e21f0964ed4eeb
AHenry79 <austinhenry7799@gmail.com>
2024-05-07 12:20:31 -0400

    Add puppy names

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import { useState } from "react";
-
+import { puppyList } from "./components/data";
+console.log("puppyList: ", puppyList);
 function App() {
-  return <></>;
+  const [puppies, setPuppies] = useState(puppyList);
+  return (
+    <>
+      <div>
+        {puppies.map((p) => {
+          return <p key={p.id}>{p.name}</p>;
+        })}
+      </div>
+    </>
+  );
 }
 
 export default App;
