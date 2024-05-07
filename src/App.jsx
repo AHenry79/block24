2f23ca228dd206aeab7ec6814415917affc96a63
AHenry79 <austinhenry7799@gmail.com>
2024-05-07 12:51:01 -0400

    Add featured puppy on click

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,33 @@ import { puppyList } from "./components/data";
 console.log("puppyList: ", puppyList);
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log(featuredPup);
   return (
     <>
       <div>
         {puppies.map((p) => {
-          return <p key={p.id}>{p.name}</p>;
+          return (
+            <p
+              key={p.id}
+              onClick={() => {
+                setFeatPupId(p.id);
+              }}
+            >
+              {p.name}
+            </p>
+          );
         })}
+        {featPupId && (
+          <div>
+            <h2>{featuredPup.name}</h2>
+            <ul>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+          </div>
+        )}
       </div>
     </>
   );
